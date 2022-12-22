9aa620800bcc30917741237c5254a16c8d391c79
Aman <amanrazor111@gmail.com>
2022-12-22 09:52:00 +0530

    1

---
+++ Lab01 Intro and Variables/01-variable declaration.js
@@ -0,0 +1,10 @@
+var x =5 
+{
+var x=10
+}
+
+let y= 16
+
+const a=100
+
+console.log(x,y,a)
---
+++ Lab01 Intro and Variables/02-Data-types.js
@@ -0,0 +1,10 @@
+let a= null          //null
+let b = 123          //number
+let c= "Symbol"      //symbol
+let d= undefined     //undefined
+let e = true         //bool
+let f = 00111        //byte
+
+
+console.log(a,b,c,d,e,f)
+console.log(typeof d)
