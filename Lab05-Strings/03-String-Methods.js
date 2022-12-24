6c33265cd4aafa541c9f023ba045960b6ee6c10a
amankr158 <amanrazor111@gmail.com>
2022-12-24 16:35:52 +0530

    55

--- Lab05-Strings/03-String-Methods.js
+++ Lab05-Strings/03-String-Methods.js
@@ -1,6 +1,11 @@
-let name="Aman"
+let x="aman";
 
 //Length Function
-console.log(name.length)
+console.log(x.length)
 
-//
+//Escape character if we want to insert (')or(") in sentences
+console.log("aman\'kumar")
+
+//toUppercase & Lower
+console.log(x.toUpperCase())
+console.log(x.toLowerCase())
---
+++ Lab05-Strings/04-String-Slice-op.js
@@ -0,0 +1,9 @@
+//slice method
+let x="aman"
+console.log(x.slice(0,3))
+console.log(x.slice(2))
+
+
+//Replace method used to replace strings
+let y ="aman kumar"
+console.log(y.replace("kumar","Singh"))
