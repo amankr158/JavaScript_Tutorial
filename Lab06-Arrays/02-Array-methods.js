4c80439e57c6c40ac36a8427f3a0e0ca68b84119
amankr158 <amanrazor111@gmail.com>
2022-12-25 21:07:27 +0530

    hh

---
+++ Lab06-Arrays/02-Array-methods.js
@@ -0,0 +1,26 @@
+let n=[1,2,3,4]
+
+//to sting method
+let b =n.toString()
+console.log(b)
+
+//join method
+let a =n.join("&")
+console.log(a)
+
+//pop
+console.log(n.pop())
+console.log(n)
+
+//push
+let arr=[1,2,3,4,5,6]
+let f= arr.push(20)
+console.log(f)      //push retun length of new array
+console.log(arr)
+
+//concat
+let a1=[1,2,3,4]
+let a2=[5,6,7,8]
+let a3=[9,10,11]
+
+console.log(a1.concat(a2,a3))
