eb3a4b43dd8bff219cb8d2eca1878ef838a73676
amankr158 <amanrazor111@gmail.com>
2022-12-25 20:21:43 +0530

    8

---
+++ Lab06-Arrays/01.js
@@ -0,0 +1,10 @@
+let a = [1,2,3,4]
+console.log(a[0],a[1])
+
+const x=["harry",0,null,false]
+console.log(x[3])
+x[3]=true                   //changing value
+console.log(x[3])
+
+//finding length
+console.log(x.length)
