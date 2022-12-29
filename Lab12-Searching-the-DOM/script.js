4cfe9e7e0ac7d48b6b88993fd9025da6e6282b1e
amankr158 <amanrazor111@gmail.com>
2022-12-29 11:05:07 +0530

    cdcdcaa

---
+++ Lab12-Searching-the-DOM/script.js
@@ -0,0 +1,8 @@
+let ctitle = document.getElementById("firstcard")
+ctitle.style.color="blue"
+
+let ctitles = document.querySelectorAll(".card-title")
+ctitles[0].style.color="red"
+ctitles[1].style.color="blue"
+ctitles[2].style.color="green"
+console.log(ctitles)
